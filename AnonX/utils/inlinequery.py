00a09ce1d8a247767394906882676f671f26ee2c
jatinsinghsaini2100 <jatinsinghsaini2100@gmail.com>
2023-04-20 21:51:19 +0530

    TonyXMusic

---
+++ AnonX/utils/inlinequery.py
@@ -0,0 +1,45 @@
+from pyrogram.types import (InlineQueryResultArticle,
+                            InputTextMessageContent)
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="🙄 ᴩᴀᴜsᴇ 🙄",
+            description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/063189303c7a87cc9ddc8.jpg",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="😋 ʀᴇsᴜᴍᴇ 😋",
+            description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/8ce4830c939aef9a78ca0.jpg",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="🙂 sᴋɪᴩ 🙂",
+            description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
+            thumb_url="https://te.legra.ph/file/3c824eb1b844b77aeedf8.jpg",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="🥺 ᴇɴᴅ 🥺",
+            description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/b768bbd73fb0f4b4f4a77.jpg",
+            input_message_content=InputTextMessageContent("/end"),
+        ),
+        InlineQueryResultArticle(
+            title="🥴 sʜᴜғғʟᴇ 🥴",
+            description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+            thumb_url="https://te.legra.ph/file/6f0d7794fc7ebacf5e13a.jpg",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="🥱 ʟᴏᴏᴩ 🥱",
+            description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/bc20e9e5cdb1633266263.jpg",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
